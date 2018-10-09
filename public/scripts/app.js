b8ab4ee86baa4f2149b8cf283644f88b98e09461
Zachary Lee <zacharylhlee@gmail.com>
2018-10-09 21:08:16 +0000

    Adds renderTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(function() {
-  const tweetData = {
+  const data = [
+  {
     "user": {
       "name": "Newton",
       "avatars": {
@@ -18,15 +19,59 @@ $(function() {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
     "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+  function renderTweets(tweets) {
+    // loops through tweets
+    tweets.forEach(function(element){
+      // calls createTweetElement for each tweet
+      var $tweet = createTweetElement(element);
+
+      // takes return value and appends it to the tweets container
+      $('#tweets-container').append($tweet);
+    });
   }
 
+
   function createTweetElement(tweetData) {
+    //Save required data from tweetData
     const name = tweetData.user.name;
     const avatar = tweetData.user.avatars.small;
     const handle = tweetData.user.handle;
     const text = tweetData.content.text;
     const date = tweetData.created_at;
 
+    //Create Tweet element using jQuery
     const $tweet = $("<article>").addClass("tweet");
 
     const $tweetHeader = $("<header>").addClass("tweetHeader");
@@ -43,7 +88,6 @@ $(function() {
     const $flag = $("<i>").addClass("fas fa-flag");
     const $retweet = $("<i>").addClass("fas fa-retweet");
     const $heart = $("<i>").addClass("fas fa-heart");
-
     $icons.append($flag, $retweet, $heart);
     $footer.append($date, $icons);
 
@@ -52,6 +96,5 @@ $(function() {
     return $tweet;
   }
 
-  var $tweet = createTweetElement(tweetData);
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
 });
