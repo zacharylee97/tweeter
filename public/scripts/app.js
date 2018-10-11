b1a1bcf6cb6d72fe55d915d5ff52464ac75047c8
Zachary Lee <zacharylhlee@gmail.com>
2018-10-11 19:12:16 +0000

    Add comments to code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -30,6 +30,7 @@ function createTweetElement(tweetData) {
   const $flag = $("<i>").addClass("fas fa-flag");
   const $retweet = $("<i>").addClass("fas fa-retweet");
   const $heart = $("<i>").addClass("fas fa-heart");
+
   $icons.append($flag, $retweet, $heart);
   $footer.append($date, $icons);
 
@@ -39,34 +40,34 @@ function createTweetElement(tweetData) {
 }
 
 function renderTweets(tweets) {
-  // loops through tweets
+  //Loops through tweets
   tweets.forEach(function(element){
-    // calls createTweetElement for each tweet
+    //Calls createTweetElement for each tweet
     var $tweet = createTweetElement(element);
-    // takes return value and prepends it to the tweets container
+    //Takes return value and prepends it to the tweets container
     $('#tweets-container').prepend($tweet);
   });
 }
 
 function loadTweets() {
+  //Get tweets from database and renders tweets
   $.get("/tweets", function(data){
     renderTweets(data);
   });
 }
 
 function daysSince(date) {
-  // The number of milliseconds in one day
+  //The number of milliseconds in one day
   var ONE_DAY = 1000 * 60 * 60 * 24;
-
-  // Calculate the difference between date and today in milliseconds
+  //Calculate the difference between date and today in milliseconds
   var today = Date.now();
   var difference = today - date;
-
-  // Convert back to days and return
+  //Convert back to days and return
   return Math.round(difference/ONE_DAY);
 }
 
 function dateFormat(created) {
+  //Format date statement
   let result;
   if (created === 1) {
     result = created + " day old";
@@ -77,9 +78,9 @@ function dateFormat(created) {
 }
 
 function renderNewTweet(tweet) {
-  // calls createTweetElement for each tweet
+  //Calls createTweetElement for new tweet
   var $tweet = createTweetElement(tweet);
-  // takes return value and prepends it to the tweets container
+  //Takes return value and prepends it to the tweets container
   $('#tweets-container').prepend($tweet);
 }
 
@@ -92,17 +93,22 @@ function newTweet() {
     $(".error").text("Tweet is too long!").slideDown();
   } else {
     $(".error").slideUp();
+    //Serialize info from form
     let str = $(".target").serialize();
+    //Post new tweet to /tweets
     $.post("/tweets", str, function(data){
       $(".target").trigger("reset");
       $(".counter").text(140);
+      //Get new tweet from database and render tweet
       $.get("/tweets", function(data){
-        let tweet = renderNewTweet(data[data.length - 1]);
+        let tweet = data[data.length - 1];
+        renderNewTweet(tweet);
       });
     });
   }
 }
 
+//Load tweets from database when DOM is ready
 $(function() {
   loadTweets();
 });
